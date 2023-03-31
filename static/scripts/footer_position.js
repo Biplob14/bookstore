38ec19dee6f9c541d92c5619c4fe18c8b7b6dd40
biplob14 <biploburrahaman@gmail.com>
2023-03-31 11:46:52 +0600

    chore
    static folders fomation

---
+++ static/scripts/footer_position.js
@@ -0,0 +1,10 @@
+let footer = document.getElementById('footer_large');
+let footer_height = footer.scrollHeight;
+
+let navbar = document.getElementById('navbar');
+let navbar_height = navbar.scrollHeight;
+
+let content = document.getElementById('content');
+let window_height = window.innerHeight;
+content.style.minHeight= `${window_height - footer_height - navbar_height}px`;
+console.log(navbar_height)
