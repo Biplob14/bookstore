66319c2c62d6b443e6e1bf43d76e8e99972f5403
biplob14 <biploburrahaman@gmail.com>
2023-06-14 18:29:05 +0600

    feat: transition on delete cart item

--- cart/views.py
+++ cart/views.py
@@ -20,17 +20,14 @@ def add_to_cart(request):
         product_qty = int(request.POST.get('productqty'))
         product_obj = get_object_or_404(Product, id=product_id)
         cart.add(product=product_obj, qty=product_qty)
-        print("on add to cart.........................", product_obj)
 
         cartqty = cart.__len__()
         response = JsonResponse({'qty': cartqty})
-        print("####################", response)
         return response
 
 def cart_item_del(request):
     cart = CartManager(request)
     if request.POST.get('action') == 'post':
-        print("########### Delete item #############")
         product_id = int(request.POST.get('productid'))
         # invoke delete method from cart
         cart.delete(product=product_id)
